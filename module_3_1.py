a8113a020e659aabcf1701076220e61e1724294e
ikermi <ikermillan18@gmail.com>
2023-01-19 23:37:35 +0100

    first commit

---
+++ module_3_1.py
@@ -0,0 +1,52 @@
+positions = ['2b', '3b', 'ss', 'dh']
+players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+
+scoreboard = zip(positions, players)
+
+print(list(scoreboard))
+
+# sets
+tags = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+print(tags)
+
+query_one = 'python' in tags
+query_two = 'ruby' in tags
+
+print(query_one)
+print(query_two)
+
+tags_one = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+tags_two = {
+  'ruby',
+  'coding',
+  'tutorials',
+  'development'
+}
+
+# merged tags
+merged_tags = tags_one | tags_two
+print(merged_tags)
+
+# tags in tags_one but not in tags_two
+exclusive_to_tag_one = tags_one - tags_two
+print(exclusive_to_tag_one)
+
+# tags in tags_two but not in tags_one
+exclusive_to_tag_two =  tags_two - tags_one
+print(exclusive_to_tag_two)
+
+# tags found in both tags_one and tags_two
+universal_tags = tags_one & tags_two
+print(universal_tags)
